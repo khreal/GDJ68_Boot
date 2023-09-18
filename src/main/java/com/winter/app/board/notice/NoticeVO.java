f7dbaa71fea1ec3d88e7e1604aca1f7b253aebc7
khreal <khreall@naver.com>
2023-09-18 17:33:28 +0900

    new

---
+++ src/main/java/com/winter/app/board/notice/NoticeVO.java
@@ -0,0 +1,19 @@
+package com.winter.app.board.notice;
+
+import java.sql.Date;
+
+import com.winter.app.board.BoardVO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NoticeVO extends BoardVO{
+	
+	private Long ref;
+	private Long step;
+	private Long depth;
+
+
+}
