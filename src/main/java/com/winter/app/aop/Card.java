fb126c7f7d27c096558f98fe5cd8bd0243d62e8e
khreal <khreall@naver.com>
2023-09-20 17:42:10 +0900

    7

---
+++ src/main/java/com/winter/app/aop/Card.java
@@ -0,0 +1,23 @@
+package com.winter.app.aop;
+
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@Aspect
+public class Card {
+
+	//advice
+//	@Before("excution(* com.winter.app.aop.Transfer.*())")
+//	public void cardCheck() throws Exception {
+//		log.info("==========================================");
+//		log.info("Card Check");
+//		log.info("==========================================");
+//		
+//	}
+}
---
+++ src/main/java/com/winter/app/aop/Transfer.java
@@ -0,0 +1,21 @@
+package com.winter.app.aop;
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class Transfer {
+	
+//	//point-cut
+//	public void useBus() {
+//		log.info(" 버스 타기 ");		
+//	}
+//
+//	//point-cut
+//	public void useSubway() {
+//		log.info(" 지하철 타기 ");
+//	}
+
+}
